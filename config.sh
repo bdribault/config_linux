60d5ba3aa218936b58581a5fe9abb6f318083cd1
bdribault <b.dribault@quortex.io>
2024-10-03 09:18:17 +0000

    Update install script

--- config.sh
+++ config.sh
@@ -7,26 +7,26 @@ read EMAIL
 echo -n "graphical env ? [y/n] : "
 read GRAPHICAL
 
+echo -n "docker engine ? [y/n] : "
+read DOCKER
+
 DEFAULT_PACKET_LIST="git vim tree"
 GRAPHIC_PACKET_LIST="meld"
 
-echo "installing default packets"
+PROJECT_PATH=https://raw.githubusercontent.com/bdribault/config_linux/master
 
-sudo apt-get install -y ${DEFAULT_PACKET_LIST}
-if [[ "${GRAPHICAL}" == "y" ]]
-then
-    echo "installing graphical packets"
-    sudo apt-get install -y ${GRAPHIC_PACKET_LIST}
-fi
+sudo apt update
 
+# Install default packets
+sudo apt install -y ${DEFAULT_PACKET_LIST}
+
+# git configuration
 if [[ ! -z ${NAME} ]]
 then
-    echo "setting git user.name to ${NAME}"
     git config --global user.name "${NAME}"
 fi
 if [[ ! -z ${EMAIL} ]]
 then
-    echo "setting git user.email to ${EMAIL}"
     git config --global user.email "${EMAIL}"
 fi
 
@@ -35,3 +35,46 @@ git config --global alias.st "status"
 git config --global alias.co "checkout"
 git config --global alias.br "branch"
 git config --global alias.gone "! git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D"
+
+# Install graphical packets if needed
+if [[ "${GRAPHICAL}" == "y" ]]
+then
+    sudo apt install -y ${GRAPHIC_PACKET_LIST}
+fi
+
+# Install docker if needed
+if [[ "${DOCKER}" == "y" ]]
+then
+    # Add Docker's official GPG key:
+    sudo apt install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+
+    # Install docker
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    # Check install is ok
+    sudo docker run hello-world
+
+    # Manage Docker as a non-root user
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
+    # Install docker compose helpers
+    mkdir $HOME/bin
+    wget -P $HOME/bin $PROJECT_PATH/docker_compose_alias/dcfr
+    wget -P $HOME/bin $PROJECT_PATH/docker_compose_alias/dcl
+    wget -P $HOME/bin $PROJECT_PATH/docker_compose_alias/dcr
+    sudo chmod +x $HOME/bin/dc*
+    echo 'PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+
+    echo "You may have to logout/login or restart your VM for group membership to be re-evaluated"
+fi
